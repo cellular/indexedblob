11d984e132f3866f0ef38a345c3d6245459d4119
Felix Gnass <fgnass@cellular.de>
2020-08-03 09:32:02 +0200

    Updates

---
+++ precache-manifest.877c282cab347c5e5986ddb83a68933f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaf8e804c211bf0c8155a22b1c8acfcf",
+    "url": "/indexedblob/index.html"
+  },
+  {
+    "revision": "7c2201eba8c09857c7cd",
+    "url": "/indexedblob/static/css/main.0a46cef8.chunk.css"
+  },
+  {
+    "revision": "af140574d3cbdf8a7a0c",
+    "url": "/indexedblob/static/js/2.7d75aa17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/indexedblob/static/js/2.7d75aa17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c2201eba8c09857c7cd",
+    "url": "/indexedblob/static/js/main.c41648b1.chunk.js"
+  },
+  {
+    "revision": "14e9d662149d57807977",
+    "url": "/indexedblob/static/js/runtime-main.48dfdea4.js"
+  }
+]);
